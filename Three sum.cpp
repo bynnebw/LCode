ab3732c52e6a6466eb451e006c0b56078c021edc
bynnebw <1365584663@.com>
2017-12-13 09:30:48 +0800

    使用双指针，实现three sum算法，注意避开重复元素的方法。

--- Three sum.cpp
+++ Three sum.cpp
@@ -1,3 +1,14 @@
+//15.Three sum
+//使用双指针的方法，并没有使用查找表来进行
+//注意本实现中，避开重复元素的方法
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
+
 class Solution{
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
@@ -31,3 +42,15 @@ public:
         return rtn;
     }
 };
+
+int main(){
+    vector<int> nums = {-1, 0, 1, 2, -1, -4};
+    vector<vector<int>> res = Solution().threeSum(nums);
+    for( int i = 0; i < res.size(); i ++ ){
+        for( int j = 0; j < res[i].size(); j ++ ){
+            cout<<res[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
