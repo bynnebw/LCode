c44ef918720e2404c5cab5192ff2176f69675713
bynnebw <1365584663@.com>
2017-12-15 09:19:52 +0800

    使用查找表将4sum问题的时间复杂度降为n方级别。

---
+++ four sum.cpp
@@ -0,0 +1,37 @@
+//Leet 454 4sum II
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    int fourSumCount(vector<int>& A, vector<int>& B, vector<int>& C, vector<int>& D) {
+        unordered_map<int,int> record;
+        for( int i = 0;i < C.size();i ++)
+            for( int j = 0;j < D.size(); j ++ )
+                record[ C[i] + D[j] ] ++;
+
+        int res = 0;
+        for( int i = 0; i < A.size(); i ++ )
+            for( int j = 0;j < B.size(); j ++)
+                if( record.find(0 - A[i] - B[j]) != record.end() )
+                    res++;
+
+        return res;
+    }
+};
+
+int main(){
+
+    vector<int> A = {1,2};
+    vector<int> B = {-2,-1};
+    vector<int> C = {-1,2};
+    vector<int> D = {0,2};
+
+    int res = Solution().fourSumCount(A,B,C,D);
+    cout<<"The answer of the 4 sum problem is :"<<res<<endl;
+
+    return 0;
+}
