826d7b0e620c09a6e9b997da2b3737b2a14e84a3
bynnebw <1365584663@.com>
2017-12-27 11:04:07 +0800

    revised

--- 131.PalindromePartition.cpp
+++ 131.PalindromePartition.cpp
@@ -1,5 +1,5 @@
 //131.Palindrome Partitioning
-//The program is not finished and it remains of the recursion procedure.
+//完整版
 #include <iostream>
 #include <vector>
 #include <string>
@@ -9,24 +9,35 @@ using namespace std;
 class Solution {
 private:
     vector<vector<string>> res;
-    bool isPalindrome(const string& str){
-        if( str == "" )
-            return true;
-        for( int i = 0; i < str.size(); i ++ )
-            if( str[i] != str[str.size()-i-1] )
+    bool isPalindrome(const string& str, int start, int end){
+        while (start <= end){
+            if(str[start++] != str[end--])
                 return false;
+        }
         return true;
     }
 
-    void dfs(const string& s,int index, vector<vector<string>> res){
-        if( index == s.size() && )
+    void dfs(const string& s,int index, vector<string>& temp ,vector<vector<string>>& res){
+        if( index == s.size() ){
+            res.push_back(temp);
+            return;
+        }
+
+        for( int i = index; i < s.size(); i ++ ){
+            if( isPalindrome(s,index,i) ){
+                temp.push_back(s.substr(index,i-index+1));
+                dfs(s,i+1,temp,res);
+                temp.pop_back();
+            }
+        }
     }
 public:
     vector<vector<string>> partition(string s) {
         res.clear();
-        if( s == "")
+        vector<string> temp;
+        if( s.empty() )
             return res;
-        dfs(s,0,res);
+        dfs(s,0,temp,res);
         return res;
     }
 };
@@ -34,10 +45,11 @@ public:
 int main(){
     string s = "aab";
     vector<vector<string>> res = Solution().partition(s);
-    for( int i = 0; i < res.size(); i ++ )
+    for( int i = 0; i < res.size(); i ++ ){
         for( int j = 0; j < res[i].size(); j ++ )
             cout<<res[i][j]<<" ";
-    cout<<endl;
+        cout<<endl;
+    }
 
     return 0;
 }
